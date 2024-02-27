1fede255888f17a1fc5a08be34c6816f54c159c6
JC-Berkey <98120593+JC-Berkey@users.noreply.github.com>
2024-02-27 16:33:48 -0600

    Up to III

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,12 +1,14 @@
-/* Map of GeoJSON data from MegaCities.geojson */
-//declare map var in global scope
+//declare map variable globally so all functions have access
 var map;
-//function to instantiate the Leaflet map
+var minValue;
+
+//step 1 create map
 function createMap(){
+
     //create the map
     map = L.map('map', {
         center: [0, 0],
-        zoom: 3
+        zoom: 2
     });
 
     //add OSM base tilelayer
@@ -17,31 +19,109 @@ function createMap(){
     //call getData function
     getData(map);
 };
-function onEachFeature(feature, layer) {
-    //no property named popupContent; instead, create html string with all properties
-    var popupContent = "";
-    if (feature.properties) {
-        //loop to add feature property names and values to html string
-        for (var property in feature.properties){
-            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+
+function calcMinValue(data){
+    //create empty array to store all data values
+    var allValues = [];
+    //loop through each city
+    for(var city of data.features){
+        //loop through each year
+        for(var year = 1985; year <= 2015; year+=5){
+              //get population for current year
+              var value = city.properties["Pop_"+ String(year)];
+              //add value to array
+              allValues.push(value);
         }
-        layer.bindPopup(popupContent);
+    }
+    //get minimum value of our array
+    var minValue = Math.min(...allValues)
+
+    return minValue;
+}
+
+//calculate the radius of each proportional symbol
+function calcPropRadius(attValue) {
+    //constant factor adjusts symbol sizes evenly
+    var minRadius = 5;
+    //Flannery Appearance Compensation formula
+    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+
+    return radius;
+};
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = "Pop_2015";
+
+    //create marker options
+    var options = {
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
     };
+
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>City:</b> " + feature.properties.City + "</p><p><b>" + attribute + ":</b> " + feature.properties[attribute] + "</p>";
+
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius) 
+    });
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
 };
 
-//function to retrieve the data and place it on the map
+//Add circle markers for point features to the map
+function createPropSymbols(data){
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: pointToLayer
+    }).addTo(map);
+};
+
+
+function createSequenceControls(){
+    //create range input element (slider)
+    var slider = "<input class='range-slider' type='range'></input>";
+    document.querySelector("#panel").insertAdjacentHTML('beforeend',slider);
+
+    //set slider attributes
+    document.querySelector(".range-slider").max = 6;
+    document.querySelector(".range-slider").min = 0;
+    document.querySelector(".range-slider").value = 0;
+    document.querySelector(".range-slider").step = 1;
+
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="reverse"></button>');
+    document.querySelector('#panel').insertAdjacentHTML('beforeend','<button class="step" id="forward"></button>');
+    document.querySelector('#reverse').insertAdjacentHTML('beforeend',"<img src='img/reverse.png'>")
+    document.querySelector('#forward').insertAdjacentHTML('beforeend',"<img src='img/forward.png'>")
+};
+
+//Import GeoJSON data
 function getData(map){
     //load the data
-    fetch("data/GDP.geojson")
+    fetch("data/MegaCities.geojson")
         .then(function(response){
             return response.json();
         })
         .then(function(json){
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(json, {
-                onEachFeature: onEachFeature
-            }).addTo(map);
-        })  
+            minValue = calcMinValue(json);
+            //call function to create proportional symbols
+            createPropSymbols(json);
+            createSequenceControls();
+        })
 };
 
 document.addEventListener('DOMContentLoaded',createMap)
